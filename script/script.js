72fa236f260a6e5016470c2611abe3783c4fe3a6
Sangwoo Park <sangwopark19@gmail.com>
2024-05-18 16:49:10 +0900

    feat: 연산자에 따라 다른함수를 호출하는 함수 구현

--- script/script.js
+++ script/script.js
@@ -18,3 +18,14 @@ let firstNum;
 let operator;
 let secondNum;
 
+function operate(firstNum, operator, secondNum) {
+  if (operator === "+") {
+    add(firstNum, secondNum);
+  } else if (operator === "-") {
+    subtract(firstNum, secondNum);
+  } else if (operator === "*") {
+    multiply(firstNum, secondNum);
+  } else if (operator === "/") {
+    divide(a, b);
+  }
+}
